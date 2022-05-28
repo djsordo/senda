e3535ac54168465b84c7abf864974dbd1fcb5754
Superman Ha Muerto <superman_ha_muerto@yahoo.com>
2022-05-28 21:47:17 +0200

    determinacion del area donde ha hecho click el usuario

--- src/app/porteria/porteria.component.spec.ts
+++ src/app/porteria/porteria.component.spec.ts
@@ -3,13 +3,13 @@ import { IonicModule } from '@ionic/angular';
 
 import { PorteriaComponent } from './porteria.component';
 
-describe('PorteriaComponent', () => {
+fdescribe('PorteriaComponent', () => {
   let component: PorteriaComponent;
   let fixture: ComponentFixture<PorteriaComponent>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ PorteriaComponent ],
+      declarations: [PorteriaComponent],
       imports: [IonicModule.forRoot()]
     }).compileComponents();
 
@@ -21,4 +21,57 @@ describe('PorteriaComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('polygon1', () => {
+
+    let polygon = [[0, 0], [10, 10], [10, 0], [0, 10]];
+
+    expect(component.isPointInsidePolygon([20, 20], polygon)
+    ).toBeFalse();
+    expect(component.isPointInsidePolygon([5, 5], polygon)
+    ).toBeTrue();
+
+  });
+
+  it('polygon2', () => {
+
+    let polygon2 = [[5, 5], [0, 0], [5, 0]];
+    expect(component.isPointInsidePolygon([3, 3], polygon2)
+    ).toBeTrue();
+    expect(component.isPointInsidePolygon([5, 1], polygon2)
+    ).toBeTrue();
+    expect(component.isPointInsidePolygon([8, 1], polygon2)
+    ).toBeFalse();
+
+  });
+
+  it('bizarre polygon', () => {
+
+    let bizarre = [[4, 2], [5, 2], [5, 6], [3, 6], [3, 7],
+    [6, 7], [6, 8], [1, 8], [1, 6], [2, 6],
+    [2, 5], [1, 5], [1, 4], [2, 4], [2, 3],
+    [3, 3], [3, 4], [4, 4]];
+    let pointsInside = [[4.5, 2.5],
+    [1.5, 4.5],
+    [2.5, 5.5],
+    [1.5, 6.5],
+    [5.5, 7.5]];
+    let pointsOutside = [[1.5, 3.5],
+    [1.5, 5.5],
+    [3.5, 3.5],
+    [5.5, 1.5],
+    [3.5, 6.5]];
+
+    for (let point of pointsInside) {
+      console.log(`test of point: ${point}`);
+      console.log(component.isPointInsidePolygon(point, bizarre));
+    }
+
+    for (let point of pointsOutside) {
+      console.log(`test of point: ${point}`);
+      expect(component.isPointInsidePolygon(point, bizarre)).toBeFalse();
+    }
+
+  });
+
 });
--- src/app/porteria/porteria.component.ts
+++ src/app/porteria/porteria.component.ts
@@ -2,9 +2,25 @@ import { Component,
           OnInit, 
           AfterViewInit,
           ViewChild, 
+          Input,
+          Output,
+          EventEmitter, 
           ElementRef } from '@angular/core';
 import { GestureController } from '@ionic/angular';
+import { NONAME } from 'dns';
 
+const X = 0; 
+const Y = 1;
+
+/**
+ * Muestra una imagen al usuario y le permite hacer click en 
+ * ciertas áreas "calientes": devolverá las áreas clickadas. 
+ * 
+ * Identificación de si un punto está dentro de un polígono 
+ * tomado de: 
+ * https://www.geeksforgeeks.org/how-to-check-if-a-given-point-lies-inside-a-polygon
+ * 
+ */
 @Component({
   selector: 'app-porteria',
   templateUrl: './porteria.component.html',
@@ -14,12 +30,22 @@ export class PorteriaComponent implements OnInit, AfterViewInit {
 
   @ViewChild("porteria", { read: ElementRef, static: true } )
   porteria : ElementRef;
+  @ViewChild("equis1", {read: ElementRef, static: true } )
+  equis1 : ElementRef; 
+  public porteriaStyle = { position: 'relative',
+                          top: 0,
+                          left: 0  };
+  @Input() areas = [];
+  @Input() equis1Style = { position : 'absolute', 
+                  left: '1px', 
+                  top : '1px',
+                  visibility: 'hidden' };
+  @Output() equis1StyleChange = new EventEmitter<object>();
 
   constructor( public gestureCtrl : GestureController ) { }
 
   async ngAfterViewInit() {
-    console.log("ngAfterViewInit start");
-    let gesture = await this.gestureCtrl.create({
+    let gesture = this.gestureCtrl.create({
       el: this.porteria.nativeElement,
       gestureName : "primera-prueba", 
       gesturePriority : 100, 
@@ -30,9 +56,149 @@ export class PorteriaComponent implements OnInit, AfterViewInit {
       onEnd : ev => { console.log("onEnd"); console.log(ev); }
     });
     gesture.enable( true );
-    console.log("ngAfterViewInit end");
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    console.log( this.areas );
+  }
+
+  public onPorteriaClick( event ){
+    this.equis1Style.left = `${event.offsetX - (this.equis1.nativeElement.width / 2)}px`;
+    this.equis1Style.top = `${event.offsetY - (this.equis1.nativeElement.height / 2)}px`;
+    this.equis1Style.visibility = 'visible';
+    
+  }
+
+  private getAreaNameOfPoint( point : Array<number>){
+    for( let area of this.areas ){
+      if( this.isPointInsidePolygon( point, area.polygon )){
+        console.log( area.name );
+        return area.name;
+      }
+      return null;
+    }
+  }
+
+  /**
+   * Dados tres puntos p, q, r <b>que estarán en la misma linea</b>
+   * esta función indica si el punto q está en el segmento pr.
+   */
+  private onSegment( p : Array<number>, 
+                     q : Array<number>, 
+                     r : Array<number> ) : boolean {
+    return (Math.min( p[X], r[X] ) <= q[X] && q[X] <= Math.max( p[Y], r[Y] ) )
+       && (Math.min( p[Y], r[Y] ) <= q[Y] && q[Y] <= Math.max( p[Y], r[Y] ) );
+  }
+
+  /**
+   * retorna 0 si los tres puntos dados están en el mismo segmento, 
+   * 1 o 2 si el punto q se encuentra en sentido de las agujas 
+   * del reloj. 
+   * 
+   * Nota: por la simulación que he efectuado, los valores 1 o 2 
+   * indicarán cuanto el punto esté por debajo o a la derecha del 
+   * segmento PR o bien arriba o a la izquierda.
+   * 
+   * Nota: <b>Esta función no trabaja bien con números en coma flotante, 
+   * sólamente con enteros.</b> 
+   */
+  private orientation( p : Array<number>, 
+                       q : Array<number>, 
+                       r : Array<number> ): 0 | 1 | 2{
+
+    let val = (q[Y] - p[Y]) * (r[X] - q[X])
+           - (q[X] - p[X]) * (r[Y] - q[Y]);
+
+    if (val == 0)
+    {
+        return 0; // collinear
+    }
+    return (val > 0) ? 1 : 2; // clock or counterclock wise
+  }
+
+  private doIntersect( p1 : Array<number>, 
+                       q1 : Array<number>, 
+                       p2 : Array<number>, 
+                       q2 : Array<number> ){
+
+    let o1 = this.orientation(p1, q1, p2);
+    let o2 = this.orientation(p1, q1, q2);
+    let o3 = this.orientation(p2, q2, p1);
+    let o4 = this.orientation(p2, q2, q1);
+
+    // General case
+    if (o1 != o2 && o3 != o4)
+    {
+        return true;
+    }
+
+    // Special Cases
+    // p1, q1 and p2 are collinear and
+    // p2 lies on segment p1q1
+    if (o1 == 0 && this.onSegment(p1, p2, q1))
+    {
+        return true;
+    }
+
+    // p1, q1 and p2 are collinear and
+    // q2 lies on segment p1q1
+    if (o2 == 0 && this.onSegment(p1, q2, q1))
+    {
+        return true;
+    }
+
+    // p2, q2 and p1 are collinear and
+    // p1 lies on segment p2q2
+    if (o3 == 0 && this.onSegment(p2, p1, q2))
+    {
+        return true;
+    }
+
+    // p2, q2 and q1 are collinear and
+    // q1 lies on segment p2q2
+    if (o4 == 0 && this.onSegment(p2, q1, q2))
+    {
+        return true;
+    }
+
+    // Doesn't fall in any of the above cases
+    return false;    
+  }
+
+  private isOdd( num ) {
+    return (num % 2) == 1;
+  }
+
+  public isPointInsidePolygon( point : Array<number>, 
+                                polygon: Array<Array<number>> ){
+    if( polygon.length < 3 ) 
+      return false; 
+    console.log( point );
+    let INFINITE = 10000; /* use of MAX_INT can cause overflow */
+    let extreme = [INFINITE, point[Y]]; 
+    let intersections = 0; 
+    let i = 0; 
+    do{
+      let next = (i+1) % polygon.length;
+      if( this.doIntersect(polygon[i], polygon[next], 
+                              point, extreme )){
+        if( this.orientation( polygon[i], point, polygon[next] ) == 0 /* collinear */ ) {
+          console.log( "COLLINEAR");
+          return this.onSegment( polygon[i], point, polygon[next] );
+        }
+        intersections++;
+      }
+      i = next;
+    }while( i != 0 );
+
+    // condición para que un punto esté dentro
+    // de un polígono es que el número de 
+    // intersecciones sea impar
+    console.log( `intersections: ${intersections}`);
+    return this.isOdd( intersections );
+  }
 
 }
+
+
+
