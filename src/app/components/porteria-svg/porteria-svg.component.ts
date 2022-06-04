7a49fe8678c910f154ed0075aa3bf304b187254c
Superman Ha Muerto <superman_ha_muerto@yahoo.com>
2022-06-04 19:02:42 +0200

    apostamos por la portería en svg

--- src/app/components/porteria-svg/porteria-svg.component.ts
+++ src/app/components/porteria-svg/porteria-svg.component.ts
@@ -4,7 +4,6 @@ import { Component,
         Input, 
         ElementRef} from '@angular/core';
 
-
 const X = 0; 
 const Y = 1;
 
@@ -25,48 +24,116 @@ export class PorteriaSvgComponent implements OnInit {
             zIndex: '100', 
             left: '0px', 
             top : '0px' };
-  private polygons = [{ 'name' : 'top_left',
-                      'points':  [[0.258009153318078, 0.147], 
-                                  [0.43421052631579, 0.147], 
-                                  [0.43421052631579, 0.362], 
-                                  [0.258009153318078, 0.362]] },
-                    { 'name' : 'top_center', 
-                      'points' : [[0.43421052631579, 0.147], 
-                                [0.609267734553776, 0.141], 
-                                [0.609267734553776, 0.362], 
-                                [0.43421052631579, 0.362]] },
-                    {'name' : 'top_right',
-                              'points' :[[0.609267734553776, 0.141], 
-                              [0.795766590389016, 0.141], 
-                              [0.795766590389016, 0.362], 
-                              [0.609267734553776, 0.362]] }
-                    ];
+  private polygons = [{ 'name' : 'arr_izq',
+                'points' : [[0.258009153318078, 0.147], 
+                [0.43421052631579, 0.147], 
+                [0.43421052631579, 0.362], 
+                [0.258009153318078, 0.362]]},
+                {'name' : 'arr_cen',
+                'points' :   [[0.43421052631579, 0.147], 
+                [0.609267734553776, 0.147], 
+                [0.609267734553776, 0.362], 
+                [0.43421052631579, 0.362]]},
+                {'name' : 'arr_der',
+                'points' :   [[0.609267734553776, 0.147], 
+                [0.795766590389016, 0.147], 
+                [0.795766590389016, 0.362], 
+                [0.609267734553776, 0.362]]},
+                {'name' : 'cen_izq',
+                'points' :   [[0.258009153318078, 0.362], 
+                [0.43421052631579, 0.362], 
+                [0.43421052631579, 0.583], 
+                [0.258009153318078, 0.582]]},
+                {'name' : 'cen_cen',
+                'points' :   [[0.43421052631579, 0.362], 
+                [0.609267734553776, 0.362], 
+                [0.609267734553776, 0.583], 
+                [0.43421052631579, 0.583]]},
+                {'name' : 'cen_der',
+                'points' :   [[0.609267734553776, 0.362], 
+                [0.795766590389016, 0.362], 
+                [0.795766590389016, 0.583], 
+                [0.609267734553776, 0.583]]},
+                {'name' : 'aba_izq',
+                'points' :   [[0.258009153318078, 0.583], 
+                [0.43421052631579, 0.583], 
+                [0.43421052631579, 0.806], 
+                [0.258009153318078, 0.806]]},
+                {'name' : 'aba_cen',
+                'points' :   [[0.43421052631579, 0.583], 
+                [0.609267734553776, 0.583], 
+                [0.609267734553776, 0.806], 
+                [0.43421052631579, 0.806]]},
+                {'name' : 'aba_der',
+                'points' :   [[0.609267734553776, 0.583], 
+                [0.795766590389016, 0.583], 
+                [0.795766590389016, 0.806], 
+                [0.609267734553776, 0.806]]},
+                {'name' : 'fuera_izq',
+                'points' : [[0, 0], 
+                [0.258009153318078, 0], 
+                [0.258009153318078, 1], 
+                [0, 1]]},
+                {'name' : 'fuera_cen',
+                'points' :   [[0.258009153318078, 0], 
+                [0.795766590389016, 0], 
+                [0.795766590389016, 0.147], 
+                [0.258009153318078, 0.147]]},
+                {'name' : 'fuera_der',
+                'points' :   [[0.795766590389016, 0], 
+                [1, 0], 
+                [1, 1], 
+                [0.795766590389016, 1]]}  ];
+  private cachedActualWidth;
+  private cachedActualHeight;
+  private cachedPolygons;
+  @Input() pointerStyle = {'visibility': 'hidden', 
+                          'stroke': '#420b0b',
+                          'strokeWidth' :5.556,
+                          'strokeLinecap' : 'round',
+                          'strokeMiterlimit' : 4};
+  pointerDots;
 
-
-
-  constructor() { }
+  constructor( ) {  }
 
   ngOnInit() {}
 
   public getPolygonsConverted(){
-    let actualWidth = this.porteriaImg.nativeElement.width;
-    let actualHeight = this.porteriaImg.nativeElement.height; 
-    let result = [];
+    if( this.cachedActualWidth !== this.porteriaImg.nativeElement.width 
+     || this.cachedActualHeight !== this.porteriaImg.nativeElement.height )
+       this.calculatePolygonsConverted();
+    return this.cachedPolygons;
+  }
+
+  public calculatePolygonsConverted(){
+    this.cachedActualWidth = this.porteriaImg.nativeElement.width;
+    this.cachedActualHeight = this.porteriaImg.nativeElement.height; 
+    this.cachedPolygons = [];
     for( let polygon of this.polygons ){
       let convertedPoints = "";
       for( let point of polygon.points ){
-        convertedPoints += `${point[X] * actualWidth}, 
-                            ${point[Y] * actualHeight} `;
+        convertedPoints += `${point[X] * this.cachedActualWidth}, 
+                            ${point[Y] * this.cachedActualHeight} `;
       }
-      result.push( { 'name' : polygon.name, 
-                     'points' : convertedPoints });
+      this.cachedPolygons.push( { 'name' : polygon.name, 
+                            'points' : convertedPoints });
     }
-    return result;
+    return this.cachedPolygons;
   }
 
   public onClickPolygon( event ) : void {
-    console.log( "area pulsada: " );
-    console.log( event );
+    if( event.srcElement.id !== "equis" ){
+      console.log( event.srcElement.id );
+      let x = event.x;
+      let y = event.y;
+      let w = 10;
+      // event.srcElement.id
+      this.pointerStyle.visibility = 'visible';
+      this.pointerDots =  `M ${x-w},${y-w} ${x+w},${y+w} 
+                            M ${x+w},${y-w} ${x-w},${y+w}`; 
+      console.log( x, y );
+      console.log( this.pointerDots );
+    }
   }
 
 }
