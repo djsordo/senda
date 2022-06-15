dff6a5aebe6379a7bc78bc8168535ae3c7cb94c4
Superman Ha Muerto <superman_ha_muerto@yahoo.com>
2022-06-16 00:01:27 +0200

    ajustes menores

--- src/app/components/microfono/microfono.component.ts
+++ src/app/components/microfono/microfono.component.ts
@@ -1,6 +1,5 @@
-import { ChangeDetectorRef, 
-    Component, 
-    OnInit } from '@angular/core';
+import { Component, 
+          OnInit } from '@angular/core';
 import { SpeechRecognition  } from '@capacitor-community/speech-recognition';
 
 
@@ -15,12 +14,11 @@ export class MicrofonoComponent implements OnInit {
   showModal = false;
   recording = false;
   lastText = '';
-  private microfonoOn = "../../../assets/mic-animation.gif";
-  private microfonoOff = "../../../assets/mic-animation-disabled.gif";
+  private microfonoOn = "./assets/mic-animation.gif";
+  private microfonoOff = "./assets/mic-animation-disabled.gif";
   public microfonoImgSrc = this.microfonoOn;
 
-
-  constructor( private changeDetectorRef : ChangeDetectorRef ) { 
+  constructor( ) { 
     SpeechRecognition.available()
       .then( value => { this.isAvailable = value['available'];
                         if( this.isAvailable )
@@ -33,7 +31,6 @@ export class MicrofonoComponent implements OnInit {
   ngOnInit() {}
 
   async startRecognition() {
-    console.log("startRecognition starts");
     this.microfonoImgSrc = this.microfonoOn; 
     this.recording = true;
     if( this.isAvailable ){
@@ -41,29 +38,20 @@ export class MicrofonoComponent implements OnInit {
         partialResults : false,
         popup : false
       }).then( result => {
-          console.log("finished speech recognition and got result");
-          console.log( result ); 
-        /*
-        en IOS el resultado se encuentra en data.matches, 
-        mientras que en Android se encuentra en data.value.
-        Esto es así para versión 2.1.0 de SpeechRecognition */
-        if( result?.matches && result.matches.length > 0 ) {
+        if( result.matches && result.matches.length > 0 ) {
+          console.log( result.matches ); 
           this.lastText = result.matches[0];
-          this.changeDetectorRef.detectChanges();
           this.stopRecognition();
         }
-        console.log("startRecognition ends");
         });
     }
   }
 
   async stopRecognition() {
-    console.log("stop recognition starts");
     this.microfonoImgSrc = this.microfonoOff;
     this.recording = false; 
     if( this.isAvailable )
       SpeechRecognition.stop().then(result => {
-        console.log("stopRecognition ends");
       });
   }
 
