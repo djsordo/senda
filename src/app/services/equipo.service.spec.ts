97be09c894901a99345d97301cdcb5cc8fd9bd5c
Raul Luna <superman_ha_muerto@yahoo.com>
2022-07-30 13:16:21 +0200

    algunas llamadas a Firestore ya me funcionan

--- src/app/services/equipo.service.spec.ts
+++ src/app/services/equipo.service.spec.ts
@@ -13,70 +13,99 @@ fdescribe( 'EquipoService', () => {
 
   let equipoService : EquipoService;
 
-  beforeEach( () => {
+  beforeAll( ( callMeOnFinish ) => {
     TestBed.configureTestingModule({
       imports: [ provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
         provideFirestore(() => getFirestore()) ]
     });
     equipoService = TestBed.inject( EquipoService );
-  });
-
-  it('should be created', () => {
-    expect( equipoService ).toBeTruthy();
-  });
-
-  it('probamos a consultar un equipo', ( endFunctionCall ) => {
-    // comenzamos creando varios equipos
+    // 
+    // creación de entorno de pruebas 
+    // 
     Promise.all([
       equipoService.addEquipo( { nombre : 'rlunaro.sandia' } as Equipo ),
       equipoService.addEquipo( { nombre : 'rlunaro.melones' } as Equipo ),
       equipoService.addEquipo( { nombre : 'rlunaro.melocotones' } as Equipo )])
         .then( ([ ref1, ref2, ref3 ]) => {
           console.log('creados los tres documentos:');
-          console.log( ref1 );
-          console.log( ref2 );
-          console.log( ref3 );
-          equipoService.queryEquipos( 'rlunaro.sandia' )
-            .then( (docList) => {
-              console.log( 'value of docList:');
-              console.log( docList );
-              expect( docList ).toBeTrue();
-              endFunctionCall();
-              Promise.all([
-                //equipoService.deleteEquipo( ref1 ),
-                //equipoService.deleteEquipo( ref2 ),
-                //equipoService.deleteEquipo( ref3 )
-              ]);
-            });
-        });
+          console.log( ref1.id );
+          console.log( ref2.id );
+          console.log( ref3.id );
+        })
+        .then( () => { 
+          console.log('ultima llamada antes de dar por finalizado el beforeAll');
+          callMeOnFinish(); });
   });
 
-  it('guardamos un equipo', ()=>{
+  afterAll( ( callMeOnFinish ) => {
+    Promise.all([
+      equipoService.deleteEquipoByName( 'rlunaro.sandia' ), 
+      equipoService.deleteEquipoByName( 'rlunaro.melones' ), 
+      equipoService.deleteEquipoByName( 'rlunaro.melocotones' ), 
+      equipoService.deleteEquipoByName( 'raul.luna.equipo' ) ])
+      .then( () => { callMeOnFinish(); });
+  });
+
+  it('should be created', () => {
+    expect( equipoService ).toBeTruthy();
+  });
+
+  it('consulta de un equipo', ( endFunctionCall ) => {
+    equipoService.getEquipoByName( 'rlunaro.sandia' )
+      .then( (docList) => {
+        docList.forEach( doc => {
+          console.log( "resultado de la query:");
+          console.log( doc.data() );
+          expect( doc.data().nombre == 'rlunaro.sandia' ).toBeTrue();
+        });
+        endFunctionCall();
+      });
+  });
+
+  it('guardamos un equipo', ( callMeOnFinish )=>{
     let equipo = equipoService.newEquipo();
     equipo.nombre = 'raul.luna.equipo';
     equipoService.addEquipo( equipo )
-      .then((value) =>  {
-          console.log( 'al crear el equipo he recibido el valor: ' );
-          console.log( value.id );
-          equipoService.deleteEquipo( value )
-            .then( (value) => {
-              console.log( 'el borrado ha ido bien' );
-              console.log( value );
-            });
+      .then((equipoRef) =>  {
+          let check = equipoService.getEquipoByName( 'raul.luna.equipo' );
+          console.log("que es equipo ref");
+          equipoService.getEquipoByRef( equipoRef )
+            .then( value => {
+              expect( value ).toBeTruthy();
+              expect( value.data ).toBeTruthy();
+              expect( value.data().nombre == 'raul.luna.equipo' ).toBeTrue();
+              check.then( checkRef => {
+                    /* AQUI ME QUEDO: getEquipoByName y getEquipoByRef no 
+                    devuelven los mismos valores: el uno devuelve una
+                    referencia a un documento, el otro devuelve 
+                    ¿una referencia a una colección de documentos??? 
+                    Tengo que plantearme qué es lo que realmente necesito
+                    en el API antes de seguir con el test, ya que no 
+                    acierto con el diseño del API que quiero */
+                    console.log( "ahora vamos con el check" );
+                    console.log( checkRef );
+                    callMeOnFinish();
+                   });
+            } );
        } )
       .catch((value) => {
           console.log( 'esto es lo que se lanza cuando falla la creación del equipo', value );
       })
   });
 
-  it('recuperamos todos los equipos', () => {
+  it('recuperamos todos los equipos', ( callMeOnFinish ) => {
     let equipo = equipoService.newEquipo();
     equipoService.getAllEQuipo()
       .then((allElements) => {
-        console.log(allElements[0]);
+        let numEquipos = 0;
+        allElements.forEach( elem => {
+          numEquipos++;
+        })
+        expect( numEquipos > 0 ).toBeTrue(); 
+        callMeOnFinish();
       })
       .catch((value) => {
-        console.log("something have happened");
+        console.error( "error on test", value );
       });
   });
 
--- src/app/services/equipo.service.ts
+++ src/app/services/equipo.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from "@angular/core";
-import { Firestore, addDoc, 
+import { Firestore, 
+          addDoc, 
+          getDoc,
+          getDocs,
           CollectionReference,
-          deleteDoc,
           collection,
           DocumentData, 
-          query} from '@angular/fire/firestore';
-import { Query} from 'firebase/firestore';
+          query,
+          where,
+          deleteDoc,
+          DocumentReference} from '@angular/fire/firestore';
 
 import { Equipo } from "../modelo/equipo";
 
@@ -29,16 +33,31 @@ export class EquipoService {
     return addDoc( this.equipoRef, equipo );
   }
 
-  async queryEquipos( nameContains : string ){
-    return query( this.equipoRef );
+  async getEquipoByRef( equipoRef : DocumentReference<DocumentData> ){
+    return getDoc( equipoRef );
   }
 
-  async deleteEquipo( document : any ){
+  async getEquipoByName( nombre : string ){
+    const q = query( this.equipoRef, where( "nombre", "==", nombre ) );
+    return getDocs(q);
+  }
+
+  async deleteEquipoByName( nombre : string ){
+    const q = query( this.equipoRef, where( "nombre", "==", nombre ));
+    let docList = getDocs( q )
+      .then( (docList) => {
+        docList.forEach( (docRef) => {
+          deleteDoc( docRef.ref );
+        })
+      })
+  }
+
+  async deleteEquipoByRef( document : any ){
     return deleteDoc( document );
   }
 
   async getAllEQuipo(){
-    return this.equipoRef;
+    return getDocs( this.equipoRef );
   }
 
 }
