685c70a7234320e7b811731163e4b8ae72a4d17b
Raul Luna <superman_ha_muerto@yahoo.com>
2022-08-10 20:16:18 +0200

    consigo sacar la lista de clubes

--- src/app/admin/clubes/admin-clubes.page.ts
+++ src/app/admin/clubes/admin-clubes.page.ts
@@ -1,4 +1,10 @@
-import { Component } from "@angular/core";
+import { Component, 
+          EventEmitter, 
+          Input, 
+          OnInit, 
+          Output } from "@angular/core";
+import { DocumentData } from "firebase/firestore";
+import { Club } from "src/app/modelo/club";
 import { ClubesService } from "src/app/services/clubes.service";
 
 
@@ -7,14 +13,21 @@ import { ClubesService } from "src/app/services/clubes.service";
   templateUrl: './admin-clubes.page.html', 
   styleUrls : ['./admin-clubes.page.scss']
 })
-export class AdminClubesPage {
+export class AdminClubesPage implements OnInit {
+
+  @Input() prueba : DocumentData[] = [];
 
   constructor( private clubesService : ClubesService ){
-
   }
 
-  async getClubes() {
-    return this.clubesService.getClubes();
+  ngOnInit(): void {
+    this.clubesService.getClubes()
+      .then( (clubList) => {
+        for( let docSnap of clubList.docs ){
+          console.log( docSnap.data() );
+          this.prueba.push( docSnap.data() );
+        }
+      });
   }
 
 }
