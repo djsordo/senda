8adf6dc3a991efe8eba054e9fafb16f73159a168
Raul Luna <superman_ha_muerto@yahoo.com>
2022-08-23 19:13:02 +0200

    finalizada primera version de mantenimiento de equipos

--- src/app/modelo/equipo.ts
+++ src/app/modelo/equipo.ts
@@ -1,13 +1,14 @@
 import { Club } from './club';
 import { Partido } from './partido';
+import { Temporada } from './temporada';
 
 export interface Equipo {
-  id: string;
+  id?: string;
   nombre: string;
   club : Club;
   categoria: string;
   genero: string;
-  temporada: string;
+  temporada: Temporada;
   partidos?: Partido[];
 }
 
