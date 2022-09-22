d90a8578d8aa94fe4ee56066c2dfc5692f1f7913
Luna Rodríguez <RLUNARO@mapfre.net>
2022-09-23 00:00:44 +0200

    final update

--- scripts/upload_secrets.js
+++ scripts/upload_secrets.js
@@ -15,15 +15,12 @@ const SCOPES = ['https://www.googleapis.com/auth/drive.file'];
 const CREDENTIALS_PATH = `${__dirname}\\..\\PRIVATE\\credentials.json`;
 const TOKEN_PATH = `${__dirname}\\..\\PRIVATE\\token.json`;
 
-function getAuthorizationAndPerformDriveOperation( callback, sourceFilename, destFilename, destContentType ){
+function getAuthorizationAndPerformDriveOperation( callback, args ){
   fs.readFile(CREDENTIALS_PATH, (err, content) => {
     if (err) return console.log('Error loading client secret file:', err);
     // Authorize a client with credentials, then call the Google Drive API.
     authorize(JSON.parse(content), 
-                  callback, 
-                  sourceFilename, 
-                  destFilename, 
-                  destContentType );
+                  callback, args );
   });  
 }
 
@@ -34,16 +31,16 @@ function getAuthorizationAndPerformDriveOperation( callback, sourceFilename, des
  * @param {Object} credentials The authorization client credentials.
  * @param {function} callback The callback to call with the authorized client.
  */
- function authorize(credentials, callback, sourceFilename, destFilename, destContentType) {
+ function authorize(credentials, callback, args ) {
   const { client_secret, client_id, redirect_uris } = credentials.installed;
   const oAuth2Client = new google.auth.OAuth2(
       client_id, client_secret, redirect_uris[0]);
 
   // Check if we have previously stored a token.
   fs.readFile(TOKEN_PATH, (err, token) => {
-      if (err) return getAccessToken(oAuth2Client, callback);
+      if (err) return getAccessToken(oAuth2Client, callback, args );
       oAuth2Client.setCredentials(JSON.parse(token));
-      callback(oAuth2Client, sourceFilename, destFilename, destContentType ); // uploadFile
+      callback(oAuth2Client, args ); // uploadFilename
   });
 }
 
@@ -53,9 +50,9 @@ function getAuthorizationAndPerformDriveOperation( callback, sourceFilename, des
  * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
  * @param {getEventsCallback} callback The callback for the authorized client.
  */
- function getAccessToken(oAuth2Client, callback) {
+ function getAccessToken(oAuth2Client, callback, args ) {
   const authUrl = oAuth2Client.generateAuthUrl({
-      access_type: 'online',
+      access_type: 'offline',
       scope: SCOPES,
   });
   console.log('Authorize this app by visiting this url:', authUrl);
@@ -73,33 +70,45 @@ function getAuthorizationAndPerformDriveOperation( callback, sourceFilename, des
               if (err) return console.error(err);
               console.log('Token stored to', TOKEN_PATH);
           });
-          callback(oAuth2Client);
+          callback(oAuth2Client, args );
       });
   });
 }
 
-function uploadFile(auth, sourceFilename, destFilename, destContentType ) {
+
+
+function uploadFilename( auth, args ) {
   const drive = google.drive({ version: 'v3', auth });
-  var fileMetadata = {
-      'name': destFilename
-  };
-  var media = {
-      mimeType: destContentType,
-      body: fs.createReadStream( sourceFilename )
-  };
-  drive.files.create({
-      resource: fileMetadata,
-      media: media,
-      fields: 'id'
-  }, function (err, res) {
-      if (err) {
-          // Handle error
-          console.log(err);
-      } else {
-          console.log('File Id: ', res.data.id);
-      }
-  });
+  drive.files.list( { q : ` name = '${args.destFilename}' ` } )
+    .then( (result, err) => { 
+      result.data.files.forEach( (fileData) => {
+        if( fileData.name === args.destFilename ){
+          drive.files.delete( { fileId : fileData.id } )
+            .then( (value, err) => {
+              console.log( `${fileData.name} deleted because it existed on drive` );
+            })
+        }
+      });
+      drive.files.create({
+        resource: {'name': args.destFilename },
+        media: {  
+              mimeType: args.destContentType,
+              body: fs.createReadStream( args.sourceFilename ) },
+        fields: 'id' }, 
+        function (err, res) {
+          if (err) {
+            // Handle error
+            console.log(err);
+          } else {
+            console.log('File Id: ', res.data.id);
+            if( fs.existsSync( args.sourceFilename ) ){
+              fs.unlinkSync( args.sourceFilename );
+            }
+          }
+      });
+    } );
 }
+
 
 /**
  * https://javascript.plainenglish.io/how-to-create-zip-files-with-node-js-505e720ceee1
@@ -108,20 +117,20 @@ function uploadFile(auth, sourceFilename, destFilename, destContentType ) {
  * @param { string } zipFile 
  */
 function createZip( sourcePaths, zipFile ){
-  const zip = new JsZip();
 
+  const zip = new JsZip();
   try{
     for( let sourcePath of sourcePaths ){
       addPathRecursivelyToZip( zip, sourcePath );
     }
     zip.generateNodeStream( { type : 'nodebuffer', streamFiles : true } )
-      .pipe( fs.createWriteStream( zipFile ) )
-      .on('finish', () => { console.log('finished'); } );
+      .pipe( fs.createWriteStream( zipFile ) );
   }catch( err ) {
     console.log( err );
   }
   
 }
+
 
 function addPathRecursivelyToZip( zip, sourcePath ){
 
@@ -130,7 +139,6 @@ function addPathRecursivelyToZip( zip, sourcePath ){
   else{
     let zipDirectory = zip.folder( path.basename( sourcePath ) );
     for( let file of  fs.readdirSync( sourcePath ) ){
-      console.log( file );
       addPathRecursivelyToZip( zipDirectory, path.join( sourcePath, file ) );
     }
   }
@@ -138,15 +146,31 @@ function addPathRecursivelyToZip( zip, sourcePath ){
 }
 
 
-
-createZip( [`${__dirname}\\..\\PRIVATE`,
-            `${__dirname}\\..\\SECRETS.md`], 
-            'rlunaro-secrets.zip' );
-getAuthorizationAndPerformDriveOperation( uploadFile, 
-        'rlunaro-secrets.zip', 
-        'otraprueba-mas.zip', 
-        'application/octet-stream' );
+function helpMessage(){
+  console.log( `Please call with node ${path.basename( process.argv[1] )} USERNAME` );
+}
 
 
-  
-            
+/**
+ * main program 
+ */
+if( process.argv.length !== 3 ){
+  helpMessage();
+}else{
+  // 0, 1, 2: depending on the day of the month (to avoid overwriting the same file over and over)
+  let iterativeDay = (new Date()).getDate() % 3;
+  let zipFileName = `secrets_${process.argv[2]}.zip`;
+  let uploadFileName = `secrets_${process.argv[2]}_${iterativeDay}.zip`;
+  createZip( [ path.join( __dirname, '..', 'PRIVATE' ),
+               path.join( __dirname, '..', 'SECRETS.md' ) ], 
+              zipFileName );
+  getAuthorizationAndPerformDriveOperation( uploadFilename, 
+  {
+    sourceFilename : zipFileName, 
+    destFilename : uploadFileName, 
+    destContentType : 'application/octet-stream'
+  } );
+}
+
+
+
