48698b50c0bf9c5480fec0832700b10cf3a57c52
Raul Luna <superman_ha_muerto@yahoo.com>
2022-10-15 17:55:38 +0200

    se elimina el incremento automatico de version al generar el apk

--- scripts/deploy_apk.js
+++ scripts/deploy_apk.js
@@ -30,8 +30,7 @@ function main(){
   });
 
   console.log( 'Subiendo código de versión....' );
-  incrementVersionMinor( config.environment_prod );
-  setVersionGradle( config.environment_prod, 'build.gradle' );
+  setCodeVersionGradle( config.environment_prod, config.build_gradle );
 
   console.log( 'Ejecutamos ', config["capacitor_sync"] );
   runCommand( config["capacitor_sync"] )
@@ -91,95 +90,28 @@ async function moveFile( sourcePath, destPath ){
 }
 
 
-function incrementVersionMinor( filePath ){
-  const versionRegexp = /(\s*version\s*:\s*['"][0-9]+\.[0-9]+\.)([0-9]+)(['"],?)/;
-  console.log( path.dirname( filePath ) );
-  console.log( path.basename( filePath ) );
-  console.log( path.extname( filePath ) );
-  
+function setCodeVersionGradle( envPath, filePath ){
+
   const backupFilePath = path.join( path.dirname( filePath ), path.basename( filePath ) + '~' );
   const tempFilePath = path.join( path.dirname( filePath ), path.basename( filePath ) + '.tmp' );
-  console.log( backupFilePath ); 
-  console.log( tempFilePath );
+
   const rl = readline.createInterface( {
     input : fs.createReadStream( filePath ), 
     crlfDelay: Infinity
   });
   let writeStream = fs.createWriteStream( tempFilePath );
   rl.on('line', (line) => {
-    let matching =  line.match( versionRegexp );
-    if( matching ){
-      let minorNumber = parseInt( matching[2] );
-      let versionLineUpdated = `${matching[1]}${minorNumber+1}${matching[3]}`;
-      console.log( `Subimos version de ${line} a ${versionLineUpdated}` );
-      writeStream.write( versionLineUpdated + '\n' );
-    }else{
-      writeStream.write( line +'\n' );
-    }
+    writeStream.write( incrementCodeVersionGradle( line ) + '\n' );
   });
   rl.on('close', () => {
-    console.log( 'closing the output stream' );
     writeStream.close();
     fs.renameSync( filePath, backupFilePath );
     fs.renameSync( tempFilePath, filePath );
   });
+
 }
 
-
-async function getVersionToReplace( envPath ){
-  const versionRegexp = /\s*version\s*:\s*['"]([0-9]+\.[0-9]+\.[0-9]+)['"],?/;
-  return new Promise( (resolve, reject) => {
-    const rl = readline.createInterface( {
-      input : fs.createReadStream( envPath ), 
-      crlfDelay: Infinity
-    });
-    rl.on('line', (line) => {
-      let matching =  line.match( versionRegexp );
-      if( matching ){
-        resolve( matching[1] );
-      }
-    });
-  });
-  
-}
-
-function setVersionGradle( envPath, filePath ){
-
-  const backupFilePath = path.join( path.dirname( filePath ), path.basename( filePath ) + '~' );
-  const tempFilePath = path.join( path.dirname( filePath ), path.basename( filePath ) + '.tmp' );
-
-  getVersionToReplace( envPath )
-    .then( (currentVersionNumber) => {
-      const rl = readline.createInterface( {
-        input : fs.createReadStream( filePath ), 
-        crlfDelay: Infinity
-      });
-      let writeStream = fs.createWriteStream( tempFilePath );
-      rl.on('line', (line) => {
-        writeStream.write( replaceVersionNumberGradle( replaceCodeGradle( line ), currentVersionNumber ) + '\n' );
-      });
-      rl.on('close', () => {
-        console.log( 'closing the output stream' );
-        writeStream.close();
-        // xjx fs.renameSync( filePath, backupFilePath );
-        // xjx fs.renameSync( tempFilePath, filePath );
-      });
-    });
-}
-
-function replaceVersionNumberGradle( line, currentVersionNumber ) {
-  const versionRegexp = /(\s*versionName\s*['"])([0-9]+\.[0-9]+\.[0-9]+)(['"])\s*/;
-  let matching =  line.match( versionRegexp );
-  if( matching ){
-    let versionLineUpdated = `${matching[1]}${currentVersionNumber}${matching[3]}`;
-    console.log( `Subimos version de ${line} a ${versionLineUpdated}` );
-    return versionLineUpdated;
-  }else{
-    return line;
-  }
-}
-
-function replaceCodeGradle( line ){
+function incrementCodeVersionGradle( line ){
   const versionRegexp = /(\s*versionCode\s*)([0-9]+)\s*/;
   let matching =  line.match( versionRegexp );
   if( matching ){
