b7ba26c4ae02aabf9f3240dad1948d1e4a3503f7
djsordo <ajvitores@gmail.com>
2022-10-17 14:54:01 +0200

    registro

--- src/app/services/equipo.service.ts
+++ src/app/services/equipo.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
 import { Firestore,
           addDoc,
           getDoc,
@@ -11,12 +11,12 @@ import { Firestore,
           deleteDoc,
           updateDoc,
           DocumentReference,
-          DocumentSnapshot, 
+          DocumentSnapshot,
           doc } from '@angular/fire/firestore';
-import { setDoc } from "firebase/firestore";
+import { setDoc } from 'firebase/firestore';
 
-import { Equipo } from "../modelo/equipo";
-import { make_id } from "./string-util";
+import { Equipo } from '../modelo/equipo';
+import { make_id } from './string-util';
 
 
 @Injectable({
@@ -26,15 +26,15 @@ export class EquipoService {
 
   private equipoRef: CollectionReference<DocumentData>;
 
-  constructor( private firestore : Firestore ){
+  constructor( private firestore: Firestore ){
     this.equipoRef = collection( this.firestore, 'equipos' );
   }
 
-  newEquipo() : Equipo {
+  newEquipo(): Equipo {
     return {} as Equipo;
   }
 
-  async getEquipos( nombre? : string ){
+  async getEquipos( nombre?: string ){
     if( nombre && nombre.trim().length > 0 ){
       return getDocs( query( this.equipoRef, where( 'nombre', '==', nombre.trim() ) ) );
     }else{
@@ -42,7 +42,17 @@ export class EquipoService {
     }
   }
 
-  async addEquipo( equipo : Equipo ){
+  async getEquiposClub( clubId?: string ){
+    if( clubId && clubId.trim().length > 0 ){
+      return getDocs( query( this.equipoRef,  where( 'club.clubId', '==', clubId.trim() ),
+                                              where( 'temporada.actual', '==', true) )
+                            );
+    }else{
+      return getDocs( query( this.equipoRef, where( 'club.clubId', '==', '' ) ) );
+    }
+  }
+
+  async addEquipo( equipo: Equipo ){
     return setDoc( doc( this.equipoRef, this.make_id( equipo ) ), equipo );
     addDoc( this.equipoRef, equipo );
   }
@@ -52,43 +62,43 @@ export class EquipoService {
               equipo.categoria,
               equipo.genero,
               equipo.club.nombre );
-  }  
+  }
 
-  async updateEquipo( docSnap : DocumentSnapshot<DocumentData>, 
-                      equipo : any ){
+  async updateEquipo( docSnap: DocumentSnapshot<DocumentData>,
+                      equipo: any ){
     return updateDoc( docSnap.ref, equipo );
   }
 
   /**
    * @deprecated - use getEquipoById instead
-   * @param equipoRef 
-   * @returns 
+   * @param equipoRef
+   * @returns
    */
-  async getEquipoByRef( equipoRef : DocumentReference<DocumentData> ){
+  async getEquipoByRef( equipoRef: DocumentReference<DocumentData> ){
     return getDoc( equipoRef );
   }
 
-  async getEquipoById( equipoId : string ){
+  async getEquipoById( equipoId: string ){
     let docRef = doc( this.equipoRef, equipoId );
     return getDoc( docRef );
   }
 
-  async getEquipoByName( nombre : string ){
-    const q = query( this.equipoRef, where( "nombre", "==", nombre ) );
+  async getEquipoByName( nombre: string ){
+    const q = query( this.equipoRef, where( 'nombre', '==', nombre ) );
     return getDocs(q);
   }
 
-  async deleteEquipoByName( nombre : string ){
-    const q = query( this.equipoRef, where( "nombre", "==", nombre ));
+  async deleteEquipoByName( nombre: string ){
+    const q = query( this.equipoRef, where( 'nombre', '==', nombre ));
     let docList = getDocs( q )
       .then( (docList) => {
         docList.forEach( (docRef) => {
           deleteDoc( docRef.ref );
-        })
-      })
+        });
+      });
   }
 
-  async deleteEquipoById( id : string ){
+  async deleteEquipoById( id: string ){
     let docRef = doc( this.equipoRef, id );
     return deleteDoc( docRef );
   }
