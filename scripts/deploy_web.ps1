10aaac001874a2b384745389a0f28ab6eda1a62a
Raul Luna <superman_ha_muerto@yahoo.com>
2022-10-31 16:18:26 +0100

    limpiamos directorio scripts

--- scripts/deploy_web.ps1
+++ scripts/deploy_web.ps1
@@ -2,5 +2,9 @@
 #
 # deploy_web.ps1 - script para desplegar la web
 #
-node yaveremos $args[0]
+
+$script = $MyInvocation.MyCommand.Path
+$scriptPath = Split-Path $script -Parent
+
+node ($scriptPath + "\deploy_web.js") $args
 
