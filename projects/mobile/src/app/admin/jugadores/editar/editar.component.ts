99aad26f2f0ac061870e682807744f3909b84da9
Raul Luna <superman_ha_muerto@yahoo.com>
2024-07-30 21:06:09 +0200

    more work on the form

--- projects/mobile/src/app/admin/jugadores/editar/editar.component.ts
+++ projects/mobile/src/app/admin/jugadores/editar/editar.component.ts
@@ -1,4 +1,14 @@
-import { Component } from "@angular/core";
+import { Component, 
+        OnDestroy, 
+        OnInit, 
+        viewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { ActivatedRoute } from "@angular/router";
+import { SecurityService } from "../../../services/security.service";
+import { Subscription } from "rxjs";
+
+
+import { Db } from "../../../services/db.service";
 
 
 @Component({
@@ -6,7 +16,63 @@ import { Component } from "@angular/core";
   templateUrl: './editar.component.html', 
   styleUrls: ['./editar.component.scss']
 })
-export class EditarComponent {
+export class EditarComponent implements OnInit, OnDestroy {
+
+  public tiposTelefono = [
+    {id: 'personal', name: 'Personal'}, 
+    {id: 'parents', name: 'De sus padres'}, 
+    {id: 'relatives', name: 'De un familiar'},
+    {id: 'other', name: 'Otro'}
+  ];
+  private paramSubscription: Subscription;
+  private datosJugador = viewChild<NgForm>('datosJugador');
+
+  constructor( private db: Db,
+               private security: SecurityService, 
+               private route : ActivatedRoute ){ }
+
+  public ngOnInit(): void {
+    this.paramSubscription = this.route.paramMap.subscribe({
+      next: (paramMap) => this.loadJugador( paramMap.get("userId") )
+    });
+
+    setTimeout( () => {
+      console.log("setTimeout");
+    }, 1 );
+  }
+
+  public ngOnDestroy(): void {
+    this.paramSubscription?.unsubscribe();
+  }
+
+  private loadJugador( jugadorId : string ){
+    console.log( "jugadorid", jugadorId );
+    this.db.getJugador( jugadorId )
+    .then((jugadorData) => {
+      console.log( jugadorData );
+      this.datosJugador().setValue({
+        "numero": jugadorData.numero, 
+        "nombre": jugadorData.nombre,
+        "genero": "", // jugadorData?.genero, 
+        "edad": "", //jugadorData?.edad,
+        "portero": jugadorData.portero, 
+        "equipos": [],
+        "telefono": "", // jugadorData?.telefono, 
+        "tipoTelefono": "", // jugadorData?.tipoTelefono, 
+        "email": null, 
+        "notas": null });
+    });
+  }
+
+  public onSubmit( datosJugador: NgForm ){
+    console.log( datosJugador.form );
+
+  }
+
+  public onCancel(){
+    this
+  }
+
 
 }
 
