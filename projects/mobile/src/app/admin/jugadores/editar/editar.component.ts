f3aa421ead1c5709af2c22c8d680a78b2d701eaf
Raul Luna <superman_ha_muerto@yahoo.com>
2024-08-04 18:41:09 +0200

    continuo trabajando en formulario jugadores

--- projects/mobile/src/app/admin/jugadores/editar/editar.component.ts
+++ projects/mobile/src/app/admin/jugadores/editar/editar.component.ts
@@ -1,4 +1,5 @@
 import { Component, 
+        HostListener, 
         OnDestroy, 
         OnInit, 
         viewChild } from "@angular/core";
@@ -9,6 +10,8 @@ import { Subscription } from "rxjs";
 
 
 import { Db } from "../../../services/db.service";
+import { Equipo } from "../../../modelo/equipo";
+import { Jugador } from "../../../modelo/jugador";
 
 
 @Component({
@@ -25,20 +28,40 @@ export class EditarComponent implements OnInit, OnDestroy {
     {id: 'other', name: 'Otro'}
   ];
   private paramSubscription: Subscription;
+  private jugadorId: string = null;
   private datosJugador = viewChild<NgForm>('datosJugador');
+  public equipos : Equipo[];
+  public windowWidth : number; 
 
   constructor( private db: Db,
                private security: SecurityService, 
                private route : ActivatedRoute ){ }
 
   public ngOnInit(): void {
-    this.paramSubscription = this.route.paramMap.subscribe({
-      next: (paramMap) => this.loadJugador( paramMap.get("userId") )
-    });
+    this.db.getEquipo()
+    .then( (equipos) => this.equipos = equipos )
+    .then( (_) => this.subscribeToParams() );
 
-    setTimeout( () => {
-      console.log("setTimeout");
-    }, 1 );
+    if( window ){
+      this.windowWidth = window.innerWidth;
+    }else
+      this.windowWidth = 1000;
+      
+  }
+  
+  private subscribeToParams(){
+    this.paramSubscription = this.route.paramMap.subscribe({
+      next: (paramMap) => {
+        if( paramMap.get("userId") ) {
+          this.loadJugador( paramMap.get("userId") );
+        }
+      }
+    });
+  }
+
+  @HostListener('window:resize', ['$event'] )
+  onResize( event ){
+    this.windowWidth = event.srcElement.innerWidth;
   }
 
   public ngOnDestroy(): void {
@@ -50,13 +73,14 @@ export class EditarComponent implements OnInit, OnDestroy {
     this.db.getJugador( jugadorId )
     .then((jugadorData) => {
       console.log( jugadorData );
+      this.jugadorId = jugadorData.id;
       this.datosJugador().setValue({
         "numero": jugadorData.numero, 
         "nombre": jugadorData.nombre,
         "genero": "", // jugadorData?.genero, 
         "edad": "", //jugadorData?.edad,
         "portero": jugadorData.portero, 
-        "equipos": [],
+        "equipos": jugadorData.equipoId,
         "telefono": "", // jugadorData?.telefono, 
         "tipoTelefono": "", // jugadorData?.tipoTelefono, 
         "email": null, 
@@ -64,13 +88,23 @@ export class EditarComponent implements OnInit, OnDestroy {
     });
   }
 
-  public onSubmit( datosJugador: NgForm ){
-    console.log( datosJugador.form );
+  public onSubmit( datosJugador: Object ){
+    console.log( datosJugador );
+    if( this.isEditMode() ){
+      datosJugador["fechaEdad"] = new Date();
+      console.log( datosJugador );
+      this.db.updateJugador( this.jugadorId, <Jugador> datosJugador );
+    }else{
+      // creation of a new player
+    }
+  }
 
+  private isEditMode() : boolean{
+    return this.jugadorId != null;
   }
 
   public onCancel(){
-    this
+    
   }
 
 
